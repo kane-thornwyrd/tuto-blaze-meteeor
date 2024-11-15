b37ba019be0fe06d872a8c64e9165ee9ff51f10a
Jean-cédric Thérond <jean.cedric.t@gmail.com>
2024-11-15 08:47:22 +0100

    finished chapter 6

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,11 +1,45 @@
-import { Template } from 'meteor/templating';
-import { TasksCollection } from '../api/TasksCollection';
-import './App.html';
+import { Template } from 'meteor/templating'
+import { ReactiveDict } from 'meteor/reactive-dict'
 
-const tasks = () => TasksCollection.find({}, { sort: { createdAt: -1 } })
+import { TasksCollection } from '../api/TasksCollection'
+import './App.html'
+import './Task.js'
+
+const HIDE_COMPLETED_STRING = "hideCompleted"
+
+const tasks = () => {
+  const instance = Template.instance();
+  const hideCompleted = instance['state'].get(HIDE_COMPLETED_STRING);
+
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+
+  return TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+    sort: { createdAt: -1 },
+  }).fetch();
+}
+
+const hideCompleted = () => Template.instance()['state'].get(HIDE_COMPLETED_STRING)
+const incompleteCount = () => {
+  const incompleteTasksCount = TasksCollection.find({ isChecked: { $ne: true } }).count();
+  return incompleteTasksCount ? `(${incompleteTasksCount})` : '';
+}
+
+
+Template.mainContainer.onCreated(function mainContainerOnCreated() {
+  this.state = new ReactiveDict();
+})
 
 Template.mainContainer.helpers({
-  tasks
+  tasks,
+  hideCompleted,
+  incompleteCount,
+})
+
+Template.mainContainer.events({
+  "click #hide-completed-button"(_, instance) {
+    const currentHideCompleted = instance.state.get(HIDE_COMPLETED_STRING)
+    instance.state.set(HIDE_COMPLETED_STRING, !currentHideCompleted)
+  }
 })
 
 Template.form.events({
